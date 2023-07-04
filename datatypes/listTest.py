81d9502b70c983db2fb92536fe2f080bab48c15b
adityapatro9 <patroaditya9@gmail.com>
2023-07-04 10:53:55 +0530

    Added list datatype initials

---
+++ datatypes/listTest.py
@@ -0,0 +1,8 @@
+number_list = [1, 2, 3, 4, 5]
+print(number_list)
+misc_list = ['Aditya', 101]
+print(misc_list)
+combined_list = number_list + misc_list
+print(combined_list)
+misc_list[0] = misc_list[0].upper()
+print(misc_list)
